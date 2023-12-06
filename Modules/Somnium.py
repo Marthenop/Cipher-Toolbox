4128689deb29842c91c8bba07a1c403955b8c525
Marthenop <87893125+Marthenop@users.noreply.github.com>
2023-12-06 09:32:05 -0500

    Update Somnium.py
    Fixed mistakes left in the decryption segment from the module conversion.

--- Modules/Somnium.py
+++ Modules/Somnium.py
@@ -1,6 +1,4 @@
 from Modules import ClrTrm
-import re
-
 
 def begin():
     Begining2 = input("Do you want to Encrypt or Decrypt?\n\n")
@@ -16,8 +14,10 @@ def begin():
         ClrTrm.clear()
 
     if Begining2 == ("Decrypt"):
-        clear()
-        message = re.findall('.', message)
+        ClrTrm.clear()
+        message = input("Enter the text you want to decrypt:\n\n")
+        dictionary = {'26': 'a', '2b': 'b', '36': 'c', '3b': 'd', '17': 'e', '4a': 'f', '27': 'g', '3a': 'h', '37': 'i', '2a': 'j', '47': 'k', '1a': 'l', '57': 'm', '0a': 'n', '08': 'o', '59': 'p', '18': 'q', '49': 'r', '28': 's', '39': 't', '38': 'u', '29': 'v', '48': 'w', '19': 'x', '58': 'y', '09': 'z', '06': '1', '56': '2', '0b': '3', '5b': '4', '4b': '5', '46': '7', '16': '8', '07': '9', '5a': '0', '+': ' '}
+        message = [message[i:i+2] for i in range(0, len(message), 2)]
         for char in message:
             message.append(dictionary.get(message[0]))
             message.remove(message[0])
